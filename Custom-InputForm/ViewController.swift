41c2036058710a14c5e20eba3684181252dcd3a9
youn2029 <cocoa8090@gmail.com>
2019-04-08 14:58:09 +0900

    프로그래밍 방식으로 레이블 추가

--- Custom-InputForm/ViewController.swift
+++ Custom-InputForm/ViewController.swift
@@ -12,8 +12,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 네비게이션 바 타이틀
+        self.navigationItem.title = "설정"
+        
+        // 이메일 레이블을 생성하고 영역과 기본 문구를 설정
+        let lblEmail = UILabel()
+        lblEmail.frame = CGRect(x: 30, y: 100, width: 100, height: 30)
+        lblEmail.text = "이메일"
+        lblEmail.font = UIFont.systemFont(ofSize: 14)    // 레이블 폰트를 설정
+        
+        self.view.addSubview(lblEmail)  // 레이블을 루트 뷰에 추가
+        
+        // 자동갱신 Label
+        let lblUpdate = UILabel()
+        lblUpdate.frame = CGRect(x: 30, y: 150, width: 100, height: 30)
+        lblUpdate.text = "자동 갱신"
+        lblUpdate.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblUpdate)
+        
+        // 갱신주기 Label
+        let lblInterval = UILabel()
+        lblInterval.frame = CGRect(x: 30, y: 200, width: 100, height: 30)
+        lblInterval.text = "갱신주기"
+        lblInterval.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblInterval)
+        
+        
+        
     }
+    
 
 
 }
